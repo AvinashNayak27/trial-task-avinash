74065a5985009e7ddbae3922ab76510f0a222b52
Avinash <avinashnayak573@gmail.com>
2024-03-08 02:08:45 +0530

    fixed data is not being updated after deployemnet

--- app/api/getUsers/route.ts
+++ app/api/getUsers/route.ts
@@ -1,7 +1,10 @@
 import prisma from "@/lib/prisma";
 
+
+export const dynamic = "force-dynamic";
+
 export async function GET() {
-    const users = await prisma.user.findMany();
+  const users = await prisma.user.findMany();
   return new Response(JSON.stringify(users), {
     status: 200, // OK status
     headers: {
